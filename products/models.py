c8dd2ff2362a2be80043e623fb7973858ffba956
Bishwajit garai <102572447+Bishwajitgarai@users.noreply.github.com>
2025-01-11 00:27:41 +0530

    test

--- products/models.py
+++ products/models.py
@@ -4,7 +4,8 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True) 
     updated_at = models.DateTimeField(auto_now=True)
-    isActive=models.BooleanField(default=True)     
+    isActive=models.BooleanField(default=True)
+    #test     
 
     def __str__(self):
         return self.name
